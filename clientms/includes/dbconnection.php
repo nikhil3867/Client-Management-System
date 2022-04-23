5b5b65ce45e50d1c485d884f94d857f6c6c015f9
nikhil3867 <104223963+nikhil3867@users.noreply.github.com>
2022-04-23 10:24:43 +0530

    Update dbconnection.php

--- clientms/includes/dbconnection.php
+++ clientms/includes/dbconnection.php
@@ -1,16 +1,31 @@
 <?php 
-// DB credentials.
-define('DB_HOST','localhost');
-define('DB_USER','root');
-define('DB_PASS','');
-define('DB_NAME','clientmsdb');
-// Establish database connection.
-try
-{
-$dbh = new PDO("mysql:host=".DB_HOST.";dbname=".DB_NAME,DB_USER, DB_PASS,array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'"));
+$whitelist = array(
+    '127.0.0.1',
+    '::1'
+);
+
+if(in_array($_SERVER['REMOTE_ADDR'], $whitelist)){
+    // DB credentials.
+    define('DB_HOST','localhost');
+    define('DB_USER','root');
+    define('DB_PASS','');
+    define('DB_NAME','library');
+    define('DB_PORT','');
 }
-catch (PDOException $e)
-{
-exit("Error: " . $e->getMessage());
+else{
+    define('DB_HOST',getenv("MYSQLHOST"));
+    define('DB_USER',getenv("MYSQLUSER"));
+    define('DB_PASS',getenv("MYSQLPASSWORD"));
+    define('DB_NAME',getenv("MYSQLDATABASE"));
+    define('DB_PORT',getenv("MYSQLPORT"));
 }
+    // Establish database connection.
+    try
+    {
+        $dbh = new PDO("mysql:host=".DB_HOST.";dbname=".DB_NAME.";port=".DB_PORT,DB_USER, DB_PASS,array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'"));
+    }
+    catch (PDOException $e)
+    {
+        exit("Error: " . $e->getMessage());
+    }
 ?>
